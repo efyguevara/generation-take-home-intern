2805a5962ab0f673ec0723825d9b5d5dc2201fac
Stefany Guevara <efyguevara@gmail.com>
2019-08-14 18:20:46 -0400

    add api google

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -1,31 +1,70 @@
 import React, { Component } from 'react';
-let mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
- 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZWZ5Z3VldmFyYSIsImEiOiJjanphZGdpemYwMHJzM2JtbmhoZmRjbWwyIn0.s8ZYRmQ8_Ogr__LylNcBfg';
-let map = new mapboxgl.Map({
-container: 'YOUR_CONTAINER_ELEMENT_ID',
-style: 'mapbox://styles/mapbox/streets-v11'
-});
+import Maps from './Maps'
+
 /*
 * Use this component as a launching-pad to build your functionality.
 *
 */
+
+
 class YourComponent extends Component {
   render() {
     return (
       <div style={divStyle}>
-        <h1> Put your solution here!</h1>
-        
+        <Maps/>
       </div>
     );
   }
 }
 
-let divStyle = {
+var divStyle = {
   border: 'red',
   borderWidth: 2,
   borderStyle: 'solid',
   padding: 20
 };
 
+
 export default YourComponent;
+
+
+// class YourComponent extends Component {
+
+
+//   render() {
+    
+
+//   let divStyle = {
+//   border: 'red',
+//   borderWidth: 2,
+//   borderStyle: 'solid',
+//   padding: 20
+// };
+//     return (
+
+//       <div classstyle={divStyle}>
+//         <h1> Put your solution here!</h1>
+
+//         <h2>
+//           Hola
+//         </h2>
+//       </div>
+//     );
+
+//   }
+// }
+
+// export default YourComponent;
+
+
+
+//API KEY GOOGLE MAPS: AIzaSyBZrbYMVmEVDK-CgM2vh0D18mN9sW_70kM
+
+
+// Uninstall this, and make sure you have create a project in firebase console
+// Run
+// sudo npm install -g firebase-cli
+// &&
+// sudo npm install -g firebase-tools 
+// Fixing deploy
+// Run firebase init
